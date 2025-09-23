29e91e77b5c9ac5f85fb4ae25fd764058f1406ea
Linka Yujra <linkayujrapoma@gmail.com>
2025-09-23 16:56:02 -0400

    Merge branch 'main' of https://github.com/Cass1310/LARAVEL

--- emprendimiento/resources/views/layouts/navigation.blade.php
+++ emprendimiento/resources/views/layouts/navigation.blade.php
@@ -12,10 +12,6 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                    
                     @auth
                         @if(Auth::user()->rol === 'residente')
                             <x-nav-link :href="route('residente.dashboard')" :active="request()->routeIs('residente.dashboard')">
@@ -157,10 +153,6 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard Principal') }}
-            </x-responsive-nav-link>
-
             @auth
                 @if(Auth::user()->rol === 'residente')
                     <x-responsive-nav-link :href="route('residente.dashboard')" :active="request()->routeIs('residente.dashboard')">
